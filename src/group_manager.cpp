5e1ee704b8db2a97679994a4958f1f5fdc4f59ae
Freddie Liardet <frederick.liardet@arm.com>
2026-05-07 10:52:21 +0100

    Add GroupManager interface
    Add public api header file
    Add more group manager tests
    
    Change-Id: Ia8a02d20b38f90e65916e05f4492a51b05f3b643
    Signed-off-by: Freddie Liardet <frederick.liardet@arm.com>

--- src/group_manager.cpp
+++ src/group_manager.cpp
@@ -1,15 +1,20 @@
 /*
- * SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+ * SPDX-FileCopyrightText: Copyright 2025-2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include "group_manager.hpp"
 #include "logging.hpp"
 
+#include <stdexcept>
+
 namespace mlsdk::scenariorunner {
 
 void GroupManager::addResourceToGroup(const Guid &group, const Guid &resource, ResourceIdType resourceIdType) {
-    _resourceToGroup.emplace(resource, group);
+    const auto [resourceIt, inserted] = _resourceToGroup.emplace(resource, group);
+    if (!inserted && resourceIt->second != group) {
+        throw std::runtime_error("Resource already belongs to a different group");
+    }
     logging::debug("addResourceToGroup count of resources: " + std::to_string(_resourceToGroup.size()) +
                    " added type: " + std::to_string(static_cast<int>(resourceIdType)));
     _groupResources[group].insert({resource, resourceIdType});
@@ -26,11 +31,10 @@ size_t GroupManager::getAliasCount(const Guid &resource) const {
 bool GroupManager::isAliased(const Guid &resource) const { return getAliasCount(resource) > 0; }
 
 bool GroupManager::hasAliasOfType(const Guid &resource, ResourceIdType resourceIdType) const {
-    const auto it = _resourceToGroup.find(resource);
-    if (it != _resourceToGroup.end()) {
+    if (const auto group = getGroupForResource(resource); group.has_value()) {
         // Group found, look for requested type
-        for ([[maybe_unused]] const auto &[_, type] : _groupResources.at(it->second)) {
-            if (type == resourceIdType) {
+        for (const auto &groupResource : _groupResources.at(*group)) {
+            if (groupResource.second == resourceIdType) {
                 return true;
             }
         }
@@ -39,15 +43,31 @@ bool GroupManager::hasAliasOfType(const Guid &resource, ResourceIdType resourceI
 }
 
 std::shared_ptr<ResourceMemoryManager> GroupManager::getMemoryManager(const Guid &resource) {
-    const auto it = _resourceToGroup.find(resource);
-    if (it != _resourceToGroup.end()) {
-        const auto &group = it->second;
+    if (const auto group = getGroupForResource(resource); group.has_value()) {
         // Only inserts if not existing
-        const auto result = _groupMemoryManagers.emplace(group, std::make_shared<ResourceMemoryManager>());
+        const auto result = _groupMemoryManagers.emplace(*group, std::make_shared<ResourceMemoryManager>());
         return result.first->second;
     }
     // Not a group, create new one
     return std::make_shared<ResourceMemoryManager>();
+}
+
+std::optional<Guid> GroupManager::getGroupForResource(const Guid &resource) const {
+    const auto it = _resourceToGroup.find(resource);
+    if (it != _resourceToGroup.end()) {
+        return it->second;
+    }
+    return std::nullopt;
+}
+
+const GroupResources &GroupManager::getGroups() const { return _groupResources; }
+
+std::vector<GroupResourceEntry> GroupManager::getResourcesInGroup(const Guid &group) const {
+    const auto it = _groupResources.find(group);
+    if (it == _groupResources.end()) {
+        return {};
+    }
+    return {it->second.begin(), it->second.end()};
 }
 
 } // namespace mlsdk::scenariorunner
--- src/tests/group_manager_tests.cpp
+++ src/tests/group_manager_tests.cpp
@@ -1,11 +1,12 @@
 /*
- * SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+ * SPDX-FileCopyrightText: Copyright 2025-2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include "group_manager.hpp"
 
 #include <gtest/gtest.h>
+#include <stdexcept>
 
 using namespace mlsdk::scenariorunner;
 
@@ -34,4 +35,36 @@ TEST(GroupManager, GroupHandling) {
     auto mmTensor = gm.getMemoryManager(tensor0);
     auto mmImage = gm.getMemoryManager(image0);
     ASSERT_EQ(mmTensor, mmImage);
+}
+
+TEST(GroupManager, DuplicateResourceRegistrationToDifferentGroupThrows) {
+    const Guid group0("group0");
+    const Guid group1("group1");
+    const Guid tensor0("tensor0");
+    GroupManager gm;
+
+    gm.addResourceToGroup(group0, tensor0, ResourceIdType::Tensor);
+
+    ASSERT_THROW(gm.addResourceToGroup(group1, tensor0, ResourceIdType::Tensor), std::runtime_error);
+}
+
+TEST(GroupManager, GroupQueries) {
+    const Guid group0("group0");
+    const Guid tensor0("tensor0");
+    const Guid image0("image0");
+    GroupManager gm;
+
+    gm.addResourceToGroup(group0, tensor0, ResourceIdType::Tensor);
+    gm.addResourceToGroup(group0, image0, ResourceIdType::Image);
+
+    const auto group = gm.getGroupForResource(tensor0);
+    ASSERT_TRUE(group.has_value());
+    ASSERT_EQ(*group, group0);
+
+    const auto groups = gm.getGroups();
+    ASSERT_EQ(groups.size(), 1U);
+    ASSERT_NE(groups.find(group0), groups.end());
+
+    const auto resources = gm.getResourcesInGroup(group0);
+    ASSERT_EQ(resources.size(), 2U);
 }
