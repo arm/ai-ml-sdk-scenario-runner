8c7ad3e5421105155beb6cf20a512ee0e3b02ac9
Finlay Fletcher <finlay.fletcher@arm.com>
2026-05-27 23:10:58 +0100

    Support VGF resource alias groups in Scenario Runner
    Read alias group metadata from VGF model resources and
    register the corresponding scenario/VGF resources with
    the existing memory group manager before setup and
    allocation. Create VGF intermediates during resource
    setup so they follow the normal alias-aware lifecycle,
    including reuse of predeclared resources.
    
    Change-Id: Ide10110bb9e970ac2b0e7782d8c00d5e4e1bfbeb
    Signed-off-by: Finlay Fletcher <finlay.fletcher@arm.com>

--- src/tests/vgf_view_tests.cpp
+++ src/tests/vgf_view_tests.cpp
@@ -20,9 +20,9 @@ namespace {
 
 class CapturingResourceCreator final : public IResourceCreator {
   public:
-    void createBuffer(Guid guid, const BufferInfo &info) override { buffers.push_back({guid, info}); }
-    void createTensor(Guid guid, const TensorInfo &info) override { tensors.push_back({guid, info}); }
-    void createImage(Guid guid, const ImageInfo &info) override { images.push_back({guid, info}); }
+    void createBuffer(Guid guid, BufferInfo info) override { buffers.push_back({guid, info}); }
+    void createTensor(Guid guid, TensorInfo info) override { tensors.push_back({guid, info}); }
+    void createImage(Guid guid, ImageInfo info) override { images.push_back({guid, info}); }
 
     std::vector<std::pair<Guid, BufferInfo>> buffers;
     std::vector<std::pair<Guid, TensorInfo>> tensors;
