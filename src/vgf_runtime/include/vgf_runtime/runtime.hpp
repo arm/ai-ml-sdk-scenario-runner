a5e7219fd4a320df273d87ddcd93a45b00c07459
Niklas Lithammer <niklas.lithammer@arm.com>
2026-08-10 08:41:49 +0200

    feat: Remove VGF runtime component
    Change-Id: I0f269f82ce4030357ac79f5cf2417e6642eb50a2
    Signed-off-by: Niklas Lithammer <niklas.lithammer@arm.com>

--- src/vgf_runtime/include/vgf_runtime/runtime.hpp
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: Copyright 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
- * SPDX-License-Identifier: Apache-2.0
- */
-#pragma once
-
-#include <vgf_runtime/session.hpp>
-#include <vgf_runtime/workload_frontends/vgf.hpp>
