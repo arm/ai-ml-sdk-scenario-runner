823400c30d49056a8b055d84021e4342f3301917
Mohammadreza Ameri Mahabadian <mohammadreza.amerimahabadian@arm.com>
2026-08-11 14:32:09 +0100

    chore: use content-based DXC patch detection
    Change-Id: Ice8b134180c4cd9db0466ce7497e554d28180424
    Signed-off-by: Mohammadreza Ameri Mahabadian <mohammadreza.amerimahabadian@arm.com>

--- cmake/dxc.cmake
+++ cmake/dxc.cmake
@@ -15,87 +15,67 @@ if(SCENARIO_RUNNER_ENABLE_HLSL_SUPPORT)
         )
     endif()
 
-    find_package(Git REQUIRED)
-
-    set(DXC_PATCH_COMMIT_MESSAGE "dxc-static-link-for-scenario-runner-28-04-2026")
-    set(DXC_PATCH_FILE "${CMAKE_CURRENT_LIST_DIR}/../patches/dxc-static-link.patch")
-
-    execute_process(
-        COMMAND
-            "${GIT_EXECUTABLE}"
-            -C
-            "${DXC_PATH}"
-            log
-            -1
-            --fixed-strings
-            --format=%H
-            --grep=${DXC_PATCH_COMMIT_MESSAGE}
-        RESULT_VARIABLE DXC_PATCH_SEARCH_RESULT
-        OUTPUT_VARIABLE DXC_PATCH_SEARCH_OUTPUT
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_VARIABLE DXC_PATCH_SEARCH_ERROR
+    set(DXC_PATCH_FILES
+        "${CMAKE_CURRENT_LIST_DIR}/../patches/dxc-static-link.patch"
     )
-    if(NOT DXC_PATCH_SEARCH_RESULT EQUAL 0)
-        message(FATAL_ERROR "Failed to query DXC patch state:\n${DXC_PATCH_SEARCH_ERROR}")
-    endif()
 
-    if(DXC_PATCH_SEARCH_OUTPUT)
-        execute_process(
-            COMMAND
-                "${GIT_EXECUTABLE}"
-                -C
-                "${DXC_PATH}"
-                apply
-                --reverse
-                --check
-                "${DXC_PATCH_FILE}"
-            RESULT_VARIABLE DXC_PATCH_REVERSE_CHECK
-            OUTPUT_VARIABLE DXC_PATCH_REVERSE_CHECK_OUTPUT
-            ERROR_VARIABLE DXC_PATCH_REVERSE_CHECK_ERROR
-        )
-        if(DXC_PATCH_REVERSE_CHECK EQUAL 0)
-            message(STATUS "DXC patch is already applied")
-        else()
-            message(FATAL_ERROR
-                "Existing DXC commit ${DXC_PATCH_COMMIT_MESSAGE} doesnt match DXC patch file\n"
-                "${DXC_PATCH_REVERSE_CHECK_OUTPUT}\n"
-                "${DXC_PATCH_REVERSE_CHECK_ERROR}"
-            )
-        endif()
-    else()
-        execute_process(
-            COMMAND
-                "${GIT_EXECUTABLE}"
-                -C
-                "${DXC_PATH}"
-                -c
-                user.name=svc_sdk
-                -c
-                user.email=svc_sdk@arm.com
-                am
-                "${DXC_PATCH_FILE}"
-            RESULT_VARIABLE DXC_APPLY_AND_COMMIT_PATCH
-            OUTPUT_VARIABLE DXC_APPLY_AND_COMMIT_PATCH_OUTPUT
-            ERROR_VARIABLE DXC_APPLY_AND_COMMIT_PATCH_ERROR
-        )
-        if(DXC_APPLY_AND_COMMIT_PATCH EQUAL 0)
+    if(DXC_PATCH_FILES)
+        find_package(Git REQUIRED)
+
+        foreach(DXC_PATCH_FILE IN LISTS DXC_PATCH_FILES)
             execute_process(
-                COMMAND "${GIT_EXECUTABLE}" -C "${DXC_PATH}" log -1 --oneline
-                OUTPUT_VARIABLE DXC_PATCH_COMMIT
-                OUTPUT_STRIP_TRAILING_WHITESPACE
+                COMMAND
+                    "${GIT_EXECUTABLE}"
+                    -C
+                    "${DXC_PATH}"
+                    apply
+                    --reverse
+                    --check
+                    "${DXC_PATCH_FILE}"
+                RESULT_VARIABLE DXC_PATCH_REVERSE_CHECK
+                OUTPUT_VARIABLE DXC_PATCH_REVERSE_CHECK_OUTPUT
+                ERROR_VARIABLE DXC_PATCH_REVERSE_CHECK_ERROR
             )
-            message(STATUS "DXC patch ${DXC_PATCH_COMMIT} applied")
-        else()
+
+            if(DXC_PATCH_REVERSE_CHECK EQUAL 0)
+                message(STATUS "DXC patch ${DXC_PATCH_FILE} is already applied")
+                continue()
+            endif()
+
             execute_process(
-                COMMAND "${GIT_EXECUTABLE}" -C "${DXC_PATH}" am --abort
-                OUTPUT_QUIET
-                ERROR_QUIET
+                COMMAND
+                    "${GIT_EXECUTABLE}"
+                    -C
+                    "${DXC_PATH}"
+                    -c
+                    user.name=svc_sdk
+                    -c
+                    user.email=svc_sdk@arm.com
+                    am
+                    "${DXC_PATCH_FILE}"
+                RESULT_VARIABLE DXC_APPLY_AND_COMMIT_PATCH
+                OUTPUT_VARIABLE DXC_APPLY_AND_COMMIT_PATCH_OUTPUT
+                ERROR_VARIABLE DXC_APPLY_AND_COMMIT_PATCH_ERROR
             )
-            message(STATUS "${DXC_APPLY_AND_COMMIT_PATCH}")
-            message(STATUS "${DXC_APPLY_AND_COMMIT_PATCH_OUTPUT}")
-            message(STATUS "${DXC_APPLY_AND_COMMIT_PATCH_ERROR}")
-            message(FATAL_ERROR "Failed to apply DXC patch")
-        endif()
+            if(DXC_APPLY_AND_COMMIT_PATCH EQUAL 0)
+                execute_process(
+                    COMMAND "${GIT_EXECUTABLE}" -C "${DXC_PATH}" log -1 --oneline
+                    OUTPUT_VARIABLE DXC_PATCH_COMMIT
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                )
+                message(STATUS "DXC patch ${DXC_PATCH_COMMIT} applied")
+            else()
+                execute_process(
+                    COMMAND "${GIT_EXECUTABLE}" -C "${DXC_PATH}" am --abort
+                    OUTPUT_QUIET
+                    ERROR_QUIET
+                )
+                message(STATUS "${DXC_APPLY_AND_COMMIT_PATCH}")
+                message(STATUS "${DXC_APPLY_AND_COMMIT_PATCH_OUTPUT}")
+                message(STATUS "${DXC_APPLY_AND_COMMIT_PATCH_ERROR}")
+                message(FATAL_ERROR "Failed to apply DXC patch ${DXC_PATCH_FILE}")
+            endif()
+        endforeach()
     endif()
 
     set(DXC_BUILD_DIR "${CMAKE_BINARY_DIR}/_deps/dxc-build-worktree")
