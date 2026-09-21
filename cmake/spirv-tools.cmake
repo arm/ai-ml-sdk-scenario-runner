d09808ce6f8ff942dcd56f3efcdcd537f88ccbeb
Mohammadreza Ameri Mahabadian <mohammadreza.amerimahabadian@arm.com>
2026-09-21 11:44:15 +0100

    build: patch SPIRV-Tools for shape-inference (#420)
    Change-Id: Ied076f35d33bd3624d475a36d834dace0f3944db
    
    Signed-off-by: Mohammadreza Ameri Mahabadian <mohammadreza.amerimahabadian@arm.com>

--- cmake/spirv-tools.cmake
+++ cmake/spirv-tools.cmake
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright 2024-2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+# SPDX-FileCopyrightText: Copyright 2024-2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -8,14 +8,77 @@ include(version)
 set(SPIRV_TOOLS_PATH "SPIRV_TOOLS-NOTFOUND" CACHE PATH "Path to SPIR-V Tools")
 set(SPIRV-Tools_VERSION "unknown")
 
-if(EXISTS ${SPIRV_TOOLS_PATH}/CMakeLists.txt)
+if(EXISTS "${SPIRV_TOOLS_PATH}/CMakeLists.txt")
+    set(SPIRV_TOOLS_PATCH_FILES
+        "${CMAKE_CURRENT_LIST_DIR}/../patches/spirv-tools-graph-shape-inference-pass.patch"
+    )
+
+    if(SPIRV_TOOLS_PATCH_FILES)
+        find_package(Git REQUIRED)
+
+        foreach(SPIRV_TOOLS_PATCH_FILE IN LISTS SPIRV_TOOLS_PATCH_FILES)
+            execute_process(
+                COMMAND
+                    "${GIT_EXECUTABLE}"
+                    -C
+                    "${SPIRV_TOOLS_PATH}"
+                    apply
+                    --reverse
+                    --check
+                    "${SPIRV_TOOLS_PATCH_FILE}"
+                RESULT_VARIABLE SPIRV_TOOLS_PATCH_REVERSE_CHECK
+                OUTPUT_VARIABLE SPIRV_TOOLS_PATCH_REVERSE_CHECK_OUTPUT
+                ERROR_VARIABLE SPIRV_TOOLS_PATCH_REVERSE_CHECK_ERROR
+            )
+
+            if(SPIRV_TOOLS_PATCH_REVERSE_CHECK EQUAL 0)
+                message(STATUS "SPIR-V Tools patch ${SPIRV_TOOLS_PATCH_FILE} is already applied")
+                continue()
+            endif()
+
+            execute_process(
+                COMMAND
+                    "${GIT_EXECUTABLE}"
+                    -C
+                    "${SPIRV_TOOLS_PATH}"
+                    -c
+                    user.name=svc_sdk
+                    -c
+                    user.email=svc_sdk@arm.com
+                    am
+                    "${SPIRV_TOOLS_PATCH_FILE}"
+                RESULT_VARIABLE SPIRV_TOOLS_APPLY_AND_COMMIT_PATCH
+                OUTPUT_VARIABLE SPIRV_TOOLS_APPLY_AND_COMMIT_PATCH_OUTPUT
+                ERROR_VARIABLE SPIRV_TOOLS_APPLY_AND_COMMIT_PATCH_ERROR
+            )
+            if(SPIRV_TOOLS_APPLY_AND_COMMIT_PATCH EQUAL 0)
+                execute_process(
+                    COMMAND "${GIT_EXECUTABLE}" -C "${SPIRV_TOOLS_PATH}" log -1 --oneline
+                    OUTPUT_VARIABLE SPIRV_TOOLS_PATCH_COMMIT
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                )
+                message(STATUS "SPIR-V Tools patch ${SPIRV_TOOLS_PATCH_COMMIT} applied")
+            else()
+                execute_process(
+                    COMMAND "${GIT_EXECUTABLE}" -C "${SPIRV_TOOLS_PATH}" am --abort
+                    OUTPUT_QUIET
+                    ERROR_QUIET
+                )
+                message(STATUS "${SPIRV_TOOLS_APPLY_AND_COMMIT_PATCH}")
+                message(STATUS "${SPIRV_TOOLS_APPLY_AND_COMMIT_PATCH_OUTPUT}")
+                message(STATUS "${SPIRV_TOOLS_APPLY_AND_COMMIT_PATCH_ERROR}")
+                message(FATAL_ERROR "Failed to apply SPIR-V Tools patch ${SPIRV_TOOLS_PATCH_FILE}")
+            endif()
+        endforeach()
+    endif()
+
     if(NOT TARGET SPIRV-Tools)
         option(SPIRV_SKIP_TESTS "" ON)
         option(SPIRV_WERROR "" OFF)
-        add_subdirectory(${SPIRV_TOOLS_PATH} spirv-tools SYSTEM EXCLUDE_FROM_ALL)
+        add_subdirectory("${SPIRV_TOOLS_PATH}" spirv-tools SYSTEM EXCLUDE_FROM_ALL)
     endif()
 
-    mlsdk_get_git_revision(${SPIRV_TOOLS_PATH} SPIRV-Tools_VERSION)
+    mlsdk_get_git_revision("${SPIRV_TOOLS_PATH}" SPIRV-Tools_VERSION)
 else()
     find_package(SPIRV-Tools REQUIRED CONFIG)
     set(SPIRV-Tools_VERSION "unknown")
