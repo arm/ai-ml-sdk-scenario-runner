b0494edb5e25935d07387e3ca518364c8e2e2b8b
Alan Liang <alan.liang@arm.com>
2026-09-23 10:16:50 +0100

    chore: expose top-level Python binding module
    Change-Id: I1b70002d226eb64b3f5e0ba737a8cf9e879c308f
    Signed-off-by: Alan Liang <alan.liang@arm.com>

--- pip_package/scenario_runner/__init__.py
+++ pip_package/scenario_runner/__init__.py
@@ -2,4 +2,4 @@
 # SPDX-FileCopyrightText: Copyright 2025-2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 # SPDX-License-Identifier: Apache-2.0
 #
-from .scenario_runner_py import *  # noqa: F401,F403
+from scenario_runner_py import *  # noqa: F401,F403
